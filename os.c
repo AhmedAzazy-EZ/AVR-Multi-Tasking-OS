8d3457eee439b16e2f66ac3687e8bad1ce0aa551
Ahmed Azazy <86133195+AhmedAzazy-EZ@users.noreply.github.com>
2021-07-04 09:14:20 -0700

    Update os.c

--- os.c
+++ os.c
@@ -28,8 +28,8 @@ void idleTask(void )
 void osInit(uint8_t ms )
 {
 	OS_listInit();
-	ThreadCreate(idleTask , &idleTCB , idleStack , 4);
-	readyList[4].numOfItems = 0;
+	ThreadCreate(idleTask , &idleTCB , idleStack , PRIORITY_NUM - 1);
+	readyList[PRIORITY_NUM - 1].numOfItems = 0;
 	pRunning = &idleTCB.TCB_Item;
 	softwareTimers_Init();
 	timerListInit();
